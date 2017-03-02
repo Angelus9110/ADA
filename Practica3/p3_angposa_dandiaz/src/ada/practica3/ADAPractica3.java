491db0b985b2a50f887a7cf823c68151b6cb2880
Angelus9110 <angelus9110@gmail.com>
2017-03-02 14:53:51 +0100

    Fichero de prueba finales

--- Practica3/p3_angposa_dandiaz/src/ada/practica3/ADAPractica3.java
+++ Practica3/p3_angposa_dandiaz/src/ada/practica3/ADAPractica3.java
@@ -1,31 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ada.practica3;
 
+import java.awt.HeadlessException;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.JOptionPane;
 
+/**
+ * @author Angel Posada
+ * @author Daniel Diaz
+ */
 public class ADAPractica3 {
 
-    public final String fileName = "entrada2.txt";
-    public static int N = 900000;
-    public static ArrayList<Integer>[] g = new ArrayList[N];
-    public int[] visited = new int[N];
-    private static ArrayList<String> nodeMatrix = new ArrayList<String>();
+    /*
+    Definimos variables estaticas que seran utilizadas en toda la clase para la
+    lectura y escritura de ficheros
+     */
+    private static File archivo = null;
+    private static FileWriter fichero = null;
 
-    public static List<List<Integer>> scc(List<Integer>[] graph) {
+    private static FileReader fr = null;
+    private static BufferedReader br = null;
+
+    private static FileReader fr2 = null;
+    private static BufferedReader br2 = null;
+
+    private static PrintWriter pw = null;
+
+    /**
+     * Ejecuta el método buscar comunidades pasandole el nombre del fichero por
+     * teclado
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        buscarComunidades(JOptionPane.showInputDialog("Introduce el nombre del fichero"));
+    }
+
+    /**
+     * Implementa el algoritmo de Kosaraju, el cual realiza la busqueda de
+     * primero en profundidad a la lista de adyacencia normal y a su inversa de
+     * tal forma que obtiene las componentes fuertemente conexas
+     *
+     * @param graph lista de adyacencia que se recorre
+     * @return lista de listas de componentes fuertemente conexas
+     */
+    public static List<List<Integer>> componentesFuertementeConexas(List<Integer>[] graph) {
         int n = graph.length;
         boolean[] used = new boolean[n];
         List<Integer> order = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             if (!used[i]) {
-                dfs(graph, used, order, i);
+                busquedaPrimeroEnProfundidad(graph, used, order, i);
             }
         }
 
@@ -46,7 +79,7 @@ public class ADAPractica3 {
         for (int u : order) {
             if (!used[u]) {
                 List<Integer> component = new ArrayList<>();
-                dfs(reverseGraph, used, component, u);
+                busquedaPrimeroEnProfundidad(reverseGraph, used, component, u);
                 components.add(component);
             }
         }
@@ -54,52 +87,130 @@ public class ADAPractica3 {
         return components;
     }
 
-    static void dfs(List<Integer>[] graph, boolean[] used, List<Integer> res, int u) {
+    /**
+     * Implementacion del algoritmo de busqueda de primero en profundidad
+     * mediante recursividad
+     *
+     * @param graph lista de adyacencia que será recorrida
+     * @param used lista de nodos visitados
+     * @param res lista de nodos por visitar
+     * @param u indice del nodo
+     */
+    public static void busquedaPrimeroEnProfundidad(List<Integer>[] graph, boolean[] used, List<Integer> res, int u) {
         used[u] = true;
         for (int v : graph[u]) {
             if (!used[v]) {
-                dfs(graph, used, res, v);
+                busquedaPrimeroEnProfundidad(graph, used, res, v);
             }
         }
         res.add(u);
     }
 
-    public void makeGraph() throws FileNotFoundException, IOException {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        BufferedReader br2 = new BufferedReader(new FileReader(fileName));
-        String str;
-        for (int i = 0; i < N; i++) {
-            g[i] = new ArrayList<Integer>(1);
-            visited[i] = 0;
-        }
-        while ((str = br.readLine()) != null) {    
-            String[] usuarios = str.split(" ");
-            if (!nodeMatrix.contains(usuarios[0])) {
-                nodeMatrix.add(usuarios[0]);
-            }
-        }
-        N = nodeMatrix.size();
-        while ((str = br2.readLine()) != null) {  
-            String[] usuarios = str.split(" ");
-            Integer i = nodeMatrix.indexOf(usuarios[0]);
-            Integer j = nodeMatrix.indexOf(usuarios[2]);
-            g[i].add(j);
-        }
-        br.close();
-    }
-    // Usage example
+    /**
+     * Inicializa las variables y los arrays, almacena los resultados leidos del
+     * fichero en una matriz de adyacencia y posteriormente en una lista de
+     * adyacencia, y se llama al metodo componentesFuertementeConexas con esta
+     * lista
+     *
+     * @param nombreFichero nombre del fichero
+     */
+    public static void buscarComunidades(String nombreFichero) {
+        try {
+            archivo = new File(nombreFichero);
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
 
-    public static void main(String[] args) throws IOException {
-        ADAPractica3 k = new ADAPractica3();
-        k.makeGraph();
-
-        List<List<Integer>> components = scc(g);
-        for (List<Integer> component : components) {
-            if (component.size() >= 3) {
-                for (Integer integer : component) {
-                    System.out.print(nodeMatrix.get(integer) + " ");
+            /*
+            Almacenamos cada letra distinta de la comunidad en un array de caracteres que nos servira para determinar
+            el tamaño de la matriz de adyacencia
+             */
+            ArrayList<String> nodeMatrix = new ArrayList<>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] palabras = line.split(" ");
+                if (!nodeMatrix.contains(palabras[0])) {
+                    nodeMatrix.add(palabras[0]);
                 }
-                System.out.println("");
+            }
+
+            // Creamos la matriz de adyacencia con el tamaño adecuado
+            int adjacenceMatrix[][] = new int[nodeMatrix.size()][nodeMatrix.size()];
+
+            /*
+            Rellenamos la diagonal principal de 0 porque son el mismo vertice, en el resto
+            de posiciones almacenamos 100 para simular el valor infinito
+             */
+            for (int i = 0; i < adjacenceMatrix.length; i++) {
+                for (int j = 0; j < adjacenceMatrix.length; j++) {
+                    if (i == j) {
+                        adjacenceMatrix[i][j] = 0;
+                    } else {
+                        adjacenceMatrix[i][j] = 100;
+                    }
+                }
+            }
+
+            fr2 = new FileReader(archivo);
+            br2 = new BufferedReader(fr2);
+
+            /*
+            Leemos cada linea del fichero y almacenamos un 1 en la posicion adecuada para indicar
+            que un vertice esta conectado con otro y en que direccion
+             */
+            while ((line = br2.readLine()) != null) {
+                String[] palabras = line.split(" ");
+                adjacenceMatrix[nodeMatrix.indexOf(palabras[0])][nodeMatrix.indexOf(palabras[2])] = 1;
+            }
+
+            /*
+            Generamos la lista de adyacencia a partir de la matriz de adyacencia que habiamos
+            generado en intentos anteriores para aprovechar que ya la teniamos hecha
+             */
+            List<Integer>[] adjacenceList = new List[adjacenceMatrix.length];
+            List<Integer> adjacent;
+
+            for (int i = 0; i < adjacenceMatrix.length; i++) {
+                adjacent = new ArrayList<>();
+                for (int j = 0; j < adjacenceMatrix.length; j++) {
+                    if (adjacenceMatrix[i][j] == 1) {
+                        adjacent.add(j);
+                    }
+                }
+                adjacenceList[i] = adjacent;
+            }
+
+            /*
+            Imprimimos las comunidades encontradas
+             */
+            fichero = new FileWriter("salida_p3_dandiaz_angposa.txt");
+            pw = new PrintWriter(fichero);
+
+            for (List<Integer> list : componentesFuertementeConexas(adjacenceList)) {
+                if (list.size() >= 3) {
+                    pw.println("COMUNIDAD:");
+                    boolean primero = true;
+                    for (Integer integer : list) {
+                        if (!primero) {
+                            pw.print(", " + nodeMatrix.get(integer));
+                        } else {
+                            pw.print(nodeMatrix.get(integer));
+                            primero = false;
+                        }
+                    }
+                    pw.println();
+                }
+            }
+        } catch (HeadlessException | IOException e) {
+            e.addSuppressed(e);
+        } finally {
+            try {
+                if (null != fr | null != fr2 | null != fichero) {
+                    fr.close();
+                    fr2.close();
+                    fichero.close();
+                }
+            } catch (IOException e2) {
+                e2.addSuppressed(e2);
             }
         }
     }
